fb61c774a62b4475d446d251ad1adef791fd4d00
bangcr <bcl0206@naver.com>
2025-03-15 14:02:22 +0900

    test: Webhook기반 CI/CD 테스트9

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
           <li>
             Get started by editing <code>app/page.tsx</code>.
           </li>
-          <li>테스트8</li>
+          <li>테스트9</li>
         </ol>
 
         <div className={styles.ctas}>
